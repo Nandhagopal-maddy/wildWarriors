21cdf73c9220ab2fdbdfc8146e39cc36bdb9af2c
Shanofar Nisha <shanofarn99@gmail.com>
2025-02-09 15:38:45 +0530

    Footer added

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+
 
 export default function Home() {
   return (
    <>
    <Header />
+   <Footer/>
    </>
   );
 }
