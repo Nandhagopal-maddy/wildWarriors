96ed75fec435b5b3550a3cd8f0ff240b5e17f8a6
Nandhagopal R <nandhamaddy007@gmail.com>
2025-02-17 22:54:20 +0530

    removed inter font

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@
 import Header from "./Header/Header";
 import About from "./About/About";
 import { useEffect } from "react";
-import { Inter } from 'next/font/google'
 import Footer from "./components/Footer";
 import Races from "./Races/Races";
 import Wwx from "./Wwx/Wwx";
@@ -11,7 +10,6 @@ import Wwinfra from "./Wwinfra/Wwinfra";
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
@@ -19,7 +17,7 @@ export default function Home() {
 
   return (
    <>
-   <div className={inter.className}>
+   <div>
    <Header />
    <About />
    <Races />
