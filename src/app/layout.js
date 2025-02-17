faa605451f73877148190e2ed96224837ce06eb1
Nandhagopal R <nandhamaddy007@gmail.com>
2025-02-17 09:20:13 +0530

    fonts removed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,5 @@
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Wild Warrior Race",
@@ -26,7 +16,7 @@ export default function RootLayout({ children }) {
   />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         {children}
       </body>
